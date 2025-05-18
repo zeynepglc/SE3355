7a35616be8539b809192183af1c55455c9e4bdd8
Gülce Özer <162319478+zeynepglc@users.noreply.github.com>
2025-05-18 20:42:14 +0300

    Update newsApi.js

--- newsApi.js
+++ newsApi.js
@@ -1,9 +1,18 @@
 export const fetchSliderNews = async () => {
     // Gerçek API varsa fetch kullanılır
     return [
-      { id: 1, title: "Kar İstanbul'da etkili oluyor", image: "https://via.placeholder.com/400x250?text=Haber+1", content: "Detaylar..." },
-      { id: 2, title: "Trump'tan açıklama", image: "https://via.placeholder.com/400x250?text=Haber+2", content: "Detaylar..." },
-      { id: 3, title: "Ekonomi zirvede", image: "https://via.placeholder.com/400x250?text=Haber+3", content: "Detaylar..." },
-      // ...toplamda 10 tane
+      return [
+      { id: 1, title: "Bakan Şimşek'ten dezenflasyon değerlendirmesi", image: "https://geoim.bloomberght.com/l/2025/05/17/ver1747495554/3748731/jpg/640x360", content: "Detaylar..." },
+      { id: 2, title: "Erdoğan'dan Merz açıklaması", image: "https://geoim.bloomberght.com/l/2025/05/16/ver1747405818/3748704/jpg/640x360", content: "Detaylar..." },
+      { id: 3, title: "Altın kayıplarının bir kısmını geri aldı", image: "https://geoim.bloomberght.com/l/2025/05/16/ver1747372989/3748649/jpg/640x360", content: "Detaylar..." },
+      { id: 4, title: "DCT'den ilk çeyrekte 151 milyon TL brüt kâr", image: "https://geoim.bloomberght.com/l/2025/05/16/ver1747404981/3748700/jpg/640x360", content: "Detaylar..." },
+      { id: 5, title: "Grafiklerle Mart enflasyonu", image: "https://geoim.bloomberght.com/2025/04/03/ver1743667482/3745324_620x349.jpg", content: "Detaylar..." },
+      { id: 6, title: "Ücret vergiyle dilim dilim azaldı ", image: "https://geoim.bloomberght.com/l/2025/05/18/ver1747555010/3748735/jpg/640x360", content: "Detaylar..." },
+      { id: 7, title: "Trump'tan Walmart'a uyarı: Fiyatları yükseltmeyin", image: "https://geoim.bloomberght.com/l/2025/05/18/ver1747553488/3748734/jpg/640x360", content: "Detaylar..." },
+      { id: 8, title: "Ekonomi ve siyaset gündemi - 18 Mayıs 2025", image: "https://geoim.bloomberght.com/l/2025/05/18/ver1747553187/3748733/jpg/640x360", content: "Detaylar..." },
+      { id: 9, title: "Liman işletmecileri ve armatörler olarak bankalardan destek bekliyoruz", image: "https://geoim.bloomberght.com/l/2025/05/18/ver1747555981/3748736/jpg/640x360", content: "Detaylar..." },
+      { id: 10, title: "Tarım alanları zirai don nedeniyle zarar gördü", image: "https://geoim.bloomberght.com/2025/04/10/ver1744294424/3746034_816x459.jpg", content: "Detaylar..." },
+    
+      
     ];
   };
