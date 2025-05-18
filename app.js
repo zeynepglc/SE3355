a715f21e8cbdc3d2b291d6ea989620ddf681750f
Gülce Özer <162319478+zeynepglc@users.noreply.github.com>
2025-05-18 21:12:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Bileşenler
+
 import StickyAds from "./components/StickyAds";
 import StickyMenu from "./components/StickyMenu";
 import SliderNews from "./components/SliderNews";
@@ -9,22 +9,22 @@ import WeatherWidget from "./components/WeatherWidget";
 import FinanceMenu from "./components/FinanceMenu";
 import HistoryPanel from "./components/HistoryPanel";
 
-// Sayfalar
+
 import NewsPage from "./pages/NewsPage";
 
 function App() {
   return (
     <Router>
-      {/* Sol ve sağa yapışan reklamlar */}
+      {}
       <StickyAds />
 
-      {/* Üst menü */}
+      {}
       <StickyMenu />
 
-      {/* Finans bilgileri (dinamik API'den) */}
+      {}
       <FinanceMenu />
 
-      {/* Sayfa rotaları */}
+      {}
       <Routes>
         <Route
           path="/"
